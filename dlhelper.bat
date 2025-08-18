1516a08912b56869a52f6eff18fdebea2e2bd7e3
Palutenfan123 <palutenfan123@timmkoo.de>
2025-08-18 17:04:44 +0200

    Update dlhelper.bat

--- dlhelper.bat
+++ dlhelper.bat
@@ -1,22 +1,74 @@
 :: T-ROM Project (palutenfan123) 2025 (Please do not delete this !! , Thanks  : ) )
 @echo off
-cls 
-echo Downloading Porttool and resources ...
-powershell.exe -NoLogo -NoProfile -Command "Invoke-WebRequest -Uri 'https://github.com/NoahDomingues/MTK-ROM-Porter/releases/download/v1.0/MTK.ROM.Porter.exe' -OutFile '%PORT%\Porttool.exe'" >nul 2>&1
-powershell.exe -NoLogo -NoProfile -Command "Invoke-WebRequest -Uri 'https://dl.timmkoo.de/trom/res/system.img' -OutFile '%SYS%'" >nul 2>&1
-powershell.exe -NoLogo -NoProfile -Command "Invoke-WebRequest -Uri 'https://dl.timmkoo.de/trom/res/boot.img' -OutFile '%BOOT%'" >nul 2>&1
+
+
 cls
-echo Done
+echo Checking if porttool already exists ...
+
+if not exist "%PORT%\Porttool.exe" (
+    echo Porttool does not exist yet , downloading...
+    powershell.exe -NoLogo -NoProfile -Command "Invoke-WebRequest -Uri 'https://github.com/NoahDomingues/MTK-ROM-Porter/releases/download/v1.0/MTK.ROM.Porter.exe' -OutFile '%PORT%\Porttool.exe'" >nul 2>&1
+    if not exist "%PORT%\Porttool.exe" (
+        echo  Failed to download Porttool.
+        pause
+        exit /b 1
+    )
+    echo Done
+)
+
+if not exist "%SYS%" if not exist "%BOOT%" (
+    echo.
+    set /p CHOICE="Do you want to use the default T-ROM system.img and boot.img? (Y/N): "
+    if /i "!CHOICE!"=="Y" (
+        echo Downloading system.img ...
+        powershell.exe -NoLogo -NoProfile -Command "Invoke-WebRequest -Uri 'https://dl.timmkoo.de/trom/res/system.img' -OutFile '%SYS%'" >nul 2>&1
+        cls
+        echo Downloading boot.img ...
+        powershell.exe -NoLogo -NoProfile -Command "Invoke-WebRequest -Uri 'https://dl.timmkoo.de/trom/res/boot.img' -OutFile '%BOOT%'" >nul 2>&1
+
+        if not exist "%SYS%" (
+            echo Failed to download system.img!
+            pause
+            exit /b 1
+        )
+        if not exist "%BOOT%" (
+            echo Failed to download boot.img!
+            pause
+            exit /b 1
+        )
+        echo Done.
+    ) else (
+        echo Using your own system.img and boot.img.
+    )
+)
+
+cls
+echo Done.
 timeout /t 2 >nul
 cls
 echo Starting Auto Porttool ...
 start "" "%PORT%\Porttool.exe"
+cls
+:detect
+cls
 echo system.img = %SYS%
-echo boot.img = %BOOT%
-echo Mode : Replace Kernel only (Recommended for Timmkoo)
-echo Output : As IMG File 
-echo Kernel : 3.10.54
+echo boot.img   = %BOOT%
+echo Mode    : Replace Kernel only (Recommended for Timmkoo)
+echo Output  : As IMG File 
+echo Kernel  : 3.10.54
 echo Chipset : MT6582
-echo The Custom ROM needs to be the exact kernel and Chipset displayed above (Only when porting for Timmkoo Oldgen)
-echo The Settings when porting for Timmkoo Must be exact as above displayed
-pause
+echo.
+echo The Custom ROM needs to match the kernel and chipset above (Timmkoo Oldgen).
+echo Settings must be exactly as displayed.
+echo This script detects when its closed and goes straight to main menu
+tasklist /FI "IMAGENAME eq python.exe" | find /I "python.exe" >nul
+if %errorlevel% neq 0 (
+    echo Closed! Back to main menu...
+    goto closed
+)
+
+goto detect
+
+:closed
+echo Done...
+cls
