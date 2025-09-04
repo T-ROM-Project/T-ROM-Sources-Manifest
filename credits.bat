89d33de46f869e155155ff35ed6d8f6331ddf595
Palutenfan123 <wailand.harri@yahoo.com>
2025-09-04 22:43:10 +0200

    Add files via upload

---
+++ credits.bat
@@ -0,0 +1,23 @@
+@echo off
+cls
+echo                            Credits:
+echo ____________________________________________________________________
+echo Noah Domingues (Porttool)
+echo Genymobile (Scrcpy)
+echo ColdWindScholar (MIO-Kitchen)
+echo zjadzhd (MIO-Kitchen)
+echo MinDeaDBlood (MIO-Kitchen)
+echo igormiguell (MIO-Kitchen)
+echo reindex-ot (MIO-Kitchen)
+echo Zurimuka07 (MIO-Kitchen)
+echo tosasitill (MIO-Kitchen)
+echo keldrion (MIO-Kitchen)
+echo komaru-meow (MIO-Kitchen)
+echo qlenlen (MIO-Kitchen)
+echo Frostleaft07 (MIO-Kitchen)
+echo agostino64 (MIO-Kitchen)
+echo ____________________________________________________________________
+echo palutenfan123 (T-Installer (This tool))
+echo ____________________________________________________________________
+echo Press any key to go back to the main menu...
+pause >nul
