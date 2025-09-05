ddd304a2341bc6214fd54f79ecf5c68cc8c51e71
Palutenfan123 <wailand.harri@yahoo.com>
2025-09-05 21:55:52 +0200

    Add files via upload

---
+++ autoflash.bat
@@ -0,0 +1,35 @@
+@echo off
+cls
+echo Checking if MTK Drivers already exists
+if not exist "%MTKCHECk%\mtk_etw_log.exe" (
+    cls
+    echo MTK Drivers does not exist yet , downloading...
+    goto mtkdriv
+)else (
+ goto reboot
+)
+:mtkdriv
+powershell.exe -NoLogo -NoProfile -Command "Invoke-WebRequest -Uri '%BASEURL%/DriverInstall.exe' -OutFile '%MDRI%\mtkdriv.exe'" >nul 2>&1
+powershell.exe -NoLogo -NoProfile -Command "Invoke-WebRequest -Uri '%BASEURL%/autostart.bat' -OutFile '%FILETEMP%\autostart.bat'" >nul 2>&1
+echo Done 
+cls
+echo Starting Driver Setup...
+timeout /t 2 >nul
+start "" "%MDRI%\mtkdriv.exe"
+:detect
+cls
+echo Waiting until the Driver is installed ...
+echo Please finish the Setup ...
+tasklist /FI "IMAGENAME eq mtkdriv.exe" | find /I "mtkdriv.exe" >nul
+if %errorlevel% neq 0 (
+    echo Done
+    goto reboot
+)
+timeout /t 2 >nul
+goto detect
+
+:reboot
+cls
+echo Adding Autostart script ...
+echo %scriptPath% >path.txt
+start /b cmd /c "timeout /t 2 & "
