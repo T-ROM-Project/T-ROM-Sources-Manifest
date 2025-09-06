6a5c656641310d6e93a305d789e63eb1fe3044e2
Palutenfan123 <wailand.harri@yahoo.com>
2025-09-06 14:54:15 +0200

    Add files via upload

---
+++ T-Start.bat
@@ -0,0 +1,28 @@
+@echo off
+set "VER=1.7"
+echo TTTTTTT   RRRRRR   OOOOOO   M     M  
+echo    T      R     R  O    O   MM   MM  
+echo    T      RRRRRR   O    O   M M M M  
+echo    T      R   R    O    O   M  M  M  
+echo    T      R    R   OOOOOO   M     M  
+echo ====================================
+echo Autostart Helper for T-Installer %VER%
+echo ====================================
+timeout /t 5 >nul
+cls
+echo Reading Script Path ...
+timeout /t 1 >nul
+set "ROOT=%USERPROFILE%\Desktop\trom"
+set "FILETEMP=%ROOT%\temp"
+set tempFile1=%FILETEMP%\path.txt
+set /p fileContent=<%tempFile1%
+set "T-INSTALLER=%fileContent%OTP.bat"
+cls
+echo Done 
+timeout /t 2 >nul
+cls
+echo Starting T-Installer ...
+timeout /t 1 >nul
+cd %T-INSTALLER%
+cls
+call OTP.bat
