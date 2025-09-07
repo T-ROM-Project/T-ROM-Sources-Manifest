9ba0cc48bb2ddce5769b49b3383121d0f4409348
Palutenfan123 <palutenfan123@timmkoo.de>
2025-09-07 14:55:50 +0200

    Update autoflash.bat

--- autoflash.bat
+++ autoflash.bat
@@ -3,19 +3,17 @@ cls
 echo Checking if MTK Drivers already exists
 if not exist "%MTKCHECk%\mtk_etw_log.exe" (
     cls
-    echo MTK Drivers does not exist yet , downloading...
-    goto mtkdriv
+    echo MTK Drivers does not exist yet , installing...
+    goto mtkinst
 )else (
- goto reboot
+ goto spinst
 )
-:mtkdriv
-powershell.exe -NoLogo -NoProfile -Command "Invoke-WebRequest -Uri '%BASEURL%/DriverInstall.exe' -OutFile '%MDRI%\mtkdriv.exe'" >nul 2>&1
-powershell.exe -NoLogo -NoProfile -Command "Invoke-WebRequest -Uri '%BASEURL%/autostart.bat' -OutFile '%FILETEMP%\autostart.bat'" >nul 2>&1
-echo Done 
+
+:mtkinst
 cls
 echo Starting Driver Setup...
 timeout /t 2 >nul
-start "" "%MDRI%\mtkdriv.exe"
+start "" "%FILETEMP%\mtkdriv.exe"
 :detect
 cls
 echo Waiting until the Driver is installed ...
@@ -32,5 +30,5 @@ goto detect
 cls
 echo Adding Autostart script ...
 echo %scriptPath% %~n0%~x0> "%FILETEMP%\path.txt"
-start cmd.exe /c "@echo off && echo Making final setup ... && timeout /t 2 >nul && xcopy "%FILETEMP%\T-Start.bat" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\" /Y" && start cmd.exe /c "@echo off && echo Waiting until other steps are finished && timeout /t 9 && cls && echo Rebooting... && shutdown /r /t 1 "
+start cmd.exe /c "powershell -WindowStyle Minimized -Command "Start-Sleep -Seconds 1" && @echo off && echo Making final setup ... && timeout /t 2 >nul && xcopy "%FILETEMP%\T-Start.bat" "%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\" /Y" && start cmd.exe /c "@echo off && echo Waiting until other steps are finished && timeout /t 9 && cls && echo Rebooting... &&  "
 exit
