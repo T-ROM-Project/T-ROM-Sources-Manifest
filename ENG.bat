ac5fd9d720e47b1c527e9b1aa72e1146b780300e
Palutenfan123 <palutenfan123@timmkoo.de>
2025-10-25 23:00:29 +0200

    Create ENG.bat

---
+++ ENG.bat
@@ -0,0 +1,6 @@
+@echo off 
+set "part1=I am NOT RESPONSABLE for your actions."
+set "part2=I am NOT RESPONSABLE for bricked devices, dead SD cards, thermonuclear war."
+set "part3=YOU are choosing to make these modifications, and if you point the finger at me for messing up your device, I will laugh at you"
+set "part4=Do you understand ?"
+set "part5=Booting up ..."
